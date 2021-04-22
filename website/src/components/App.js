fd34866de170d88e6bf60e1689007a092d4c3604
Sean <hurleysn@bc.edu>
2021-04-22 19:26:04 -0400

    commit

--- website/src/components/App.js
+++ website/src/components/App.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect, useContext} from 'react';
 import CO2E_Emissions from './CO2Emissions';
 import GetZip from './GetZip';
 import DisplayZIP from './DisplayZIP';
+import EnvRac from './EnvRac'
 
 function App() {
 
@@ -12,10 +13,17 @@ function App() {
 
   return (
     <div className="App">
-      <GetZip setZip={setZip}/>
-      <DisplayZIP zip={zip}/>
-      {isEmptyZip ? "" : <CO2E_Emissions zip={zip}/>}
+      <div className="EnvRac">
+        <EnvRac/>
+      </div>
+      <div className="Zip">
+        <GetZip setZip={setZip}/>
+        <DisplayZIP zip={zip}/>
+        {isEmptyZip ? "" : <CO2E_Emissions zip={zip}/>}
+      </div>
+      
     </div>
+    
   );
 }
 
---
+++ website/src/components/EnvRac.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect, useContext} from 'react';
+
+
+
+function EnvRac(){
+    return(
+        <div>
+            <section className="head">Environmental racism</section> is the term is used to describe the disproportionate environmental injustice that occurs within neighborhoods that are composed by a significant number of minority groups and people of lower socio-economic status
+        </div>
+    )
+
+}
+
+export default EnvRac;
