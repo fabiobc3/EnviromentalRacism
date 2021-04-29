795e84342586d91af5fdaa8b4d5cd71a75ee9e60
Fabio Costa <fabiobc3@gmail.com>
2021-04-29 02:14:43 -0400

    debug

--- website/src/components/EnvRac.js
+++ website/src/components/EnvRac.js
@@ -1,10 +1,11 @@
-function EnvRac(){
-    return(
+import React from 'react';
+
+function EnvRac(){    
+    return (
         <div>
             <section className="head">Environmental racism</section> is the term is used to describe the disproportionate environmental injustice that occurs within neighborhoods that are composed by a significant number of minority groups and people of lower socio-economic status
         </div>
-    )
-
+      );
 }
 
 export default EnvRac;
